495186843b06787e57d36c0e470f5876877ebef6
Diego Lalanda <guaio_2014@hotmail.com>
2025-06-20 12:10:15 -0300

    configuro ping uptimerobot

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,4 @@
-// src/app.controller.ts
-import { Controller, Get, Res, Req } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,5 +8,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  /**
+   * Endpoint de Health Check para servicios de monitoreo como UptimeRobot.
+   * Responde con un status 200 OK para indicar que el servicio está activo.
+   * Es liviano y no realiza operaciones complejas.
+   */
+  
+  @Get('health')
+  @HttpCode(HttpStatus.OK) // Asegura que siempre devuelva un 200 OK
+  healthCheck(): object {
+    return { status: 'ok', timestamp: new Date().toISOString() };
   }
 }
